a49d7593630dee513ebffe06eb37ebc40c06c023
senatoprak <senatprk06@gmail.com>
2026-03-02 22:36:02 +0100

    Added second line.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Vistula");
         System.out.println ("I am Sena");
-        System.out.println("Second line");
+
     }
 }
